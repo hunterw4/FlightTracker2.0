990169ac822bd2b2ad666c84a9f61b5a2bd4dee7
hunter <hunter411w@gmail.com>
2024-01-30 16:40:33 -0700

    Finalized most if not all the HTML/CSS need to clean it up & then start working on backend

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+import datetime
+
+app = Flask(__name__)
+
+Bootstrap5(app)
+
+@app.route("/", methods=["GET","POST"])
+def home():
+    return render_template("index.html")
+
+
+
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    pass
+
+
+
+@app.route("/<location>", methods=["GET", "POST"])
+def location():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
