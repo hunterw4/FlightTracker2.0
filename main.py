b0e89bc788ec84f14c6df13077418eb24177be82
hunter <hunter411w@gmail.com>
2024-02-08 00:40:57 -0700

    Added POI button to search.html. Starting process of implementing the POI's into the index.html

--- main.py
+++ main.py
@@ -16,35 +16,53 @@ Bootstrap5(app)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    # if request.method == "POST":
+    #     city = request.form["city"]
+    #     locationsearch = LocationSearch(city)
+    #     poi_img = locationsearch.base_img()
+    #     poi_list = []
+    #     poi_list.append((city, poi_img))
+    #     return render_template("index.html", poi_list=poi_list)
     return render_template("index.html")
 
 # Current load time is 20 seconds
+
+async def imgQuery(city):
+    locationsearch = LocationSearch(city)
+    header_img = locationsearch.header_img()
+    base_img = locationsearch.base_img()
+    food_img = locationsearch.food_img()
+    architecture_img = locationsearch.architecture_img()
+    return header_img, base_img, food_img, architecture_img
+
+async def aIQuery(city):
+    gpt = openAi(city)
+    country = gpt.aiResponseCountry()
+    dynamic_p1 = gpt.aiResponseP1()
+    dynamic_p2 = gpt.aiResponseP2()
+    dynamic_p3 = gpt.aiResponseP3()
+    dynamic_h1 = gpt.aiResponseH1()
+    dynamic_h2 = gpt.aiResponseH2()
+    dynamic_h3 = gpt.aiResponseH3()
+
+    return country, dynamic_p1, dynamic_p2, dynamic_p3, dynamic_h1, dynamic_h2, dynamic_h3
+
+# Current load time is 20 seconds
 @app.route("/search", methods=["GET", "POST"])
-def search():
+async def search():
     if request.method == "POST":
         city = request.form["search"]
-        locationsearch = LocationSearch(city)
-        header_img = locationsearch.header_img()
-        base_img = locationsearch.base_img()
-        food_img = locationsearch.food_img()
-        architecture_img = locationsearch.architecture_img()
-        gpt = openAi(city)
-        country = gpt.aiResponseCountry()
-        dynamic_p1 = gpt.aiResponseP1()
-        dynamic_p2 = gpt.aiResponseP2()
-        dynamic_p3 = gpt.aiResponseP3()
-        dynamic_h1 = gpt.aiResponseH1()
-        dynamic_h2 = gpt.aiResponseH2()
-        dynamic_h3 = gpt.aiResponseH3()
-        return render_template("search.html", city=city, header_img=header_img, base_img=base_img, food_img=food_img,
+        ai_result = await aIQuery(city)
+        img_result = await imgQuery(city)
+
+        header_img, base_img, food_img, architecture_img = img_result
+        country, dynamic_p1, dynamic_p2, dynamic_p3, dynamic_h1, dynamic_h2, dynamic_h3 = ai_result
+
+    return render_template("search.html", city=city, header_img=header_img, base_img=base_img, food_img=food_img,
                                architecture_img=architecture_img, country=country, dynamic_p1=dynamic_p1, dynamic_p2=dynamic_p2, dynamic_p3=dynamic_p3, dynamic_h1=dynamic_h1,
-                               dynamic_h2=dynamic_h2, dynamic_h3=dynamic_h3)
-
-
-# @app.route("/<location>", methods=["GET", "POST"])
-# def location():
-#     pass
-
+                                dynamic_h2=dynamic_h2, dynamic_h3=dynamic_h3)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
