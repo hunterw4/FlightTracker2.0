1f031ce6822a0d67f134ee48a108faa90d33c49a
hunter <hunter411w@gmail.com>
2024-02-15 23:59:47 -0700

    Got most of standard flight API set up, still very buggy. Need to fix the expensive flight function and have a more reliable fail state for it. Need to also fix the datasuch as travel time etc.

--- API.py
+++ API.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv, dotenv_values
 from openai import OpenAI
 import asyncio
 from aiohttp import ClientSession
+from datetime import datetime as dt, timedelta as td
 
 load_dotenv()
 
@@ -90,6 +91,23 @@ class openAi:
 
         country_response = (completion.choices[0].message.content)
         return country_response
+
+    def aiResponseAirport(self):
+        client = OpenAI(
+            # This is the default and can be omitted
+            api_key=openAi.openAiKey,
+        )
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system",
+                    "content": "ONLY RESPOND WITH ONE WORD. Based off the city entered by the user return the city with the nearest airport to it, so if Tokyo has one then it would be Tokyo if Kyoto doesnt have one return the closest city that does ONLY ONE WORD WHICH WILL BE THE CITY"},
+                {"role": "user", "content": self.city}
+            ]
+        )
+
+        airport_response = (completion.choices[0].message.content)
+        return airport_response
 
     def aiResponseP1(self):
         client = OpenAI(
@@ -207,16 +225,163 @@ class flightSearch():
         iata_params = {
             "term": self.city
         }
-        response = requests.get(url=flightSearch.url, params=iata_params, headers=self.header)
+        response = requests.get(url=f"{flightSearch.url}/locations/query", params=iata_params, headers=self.header)
         data = response.json()
-        return data
-    async def search(self):
+        IATA = data["locations"][0]["id"]
+        return IATA
+    async def cheapest_search(self, IATA):
+        date = dt.now().strftime("%d/%m/%Y")
+        six_month = dt.now() + td(days=+ 180)
+        date_to = six_month.strftime("%d/%m/%Y")
+        params = {
+            "fly_from": "BOI",
+            "fly_to": IATA,
+            "date_from": date,
+            "date_to": date_to,
+            "select_airlines": "NK",
+            "select_airlines_exclude": "True",
+            "adults": 1,
+            "children": 0,
+            "max_stopovers": 4,
+            "stopover_from": "00:00",
+            "stopover_to": "04:00",
+            "curr": "USD",
+
+        }
         async with aiohttp.ClientSession() as session:
-            response = await session.get(f"{flightSearch.url}/")
-            data = response.json()
+            response = await session.get(url=f"{flightSearch.url}/v2/search", params=params, headers=self.header, ssl=False)
+            data = await response.json()
+        new_data = await self.analyze_data(data)
+        return new_data
 
 
-flight = flightSearch(city="boise")
-test = flight.IATA()
 
-print(test)
+    async def average_search(self, IATA):
+        date = dt.now().strftime("%d/%m/%Y")
+        six_month = dt.now() + td(days=+ 180)
+        date_to = six_month.strftime("%d/%m/%Y")
+        params = {
+            "fly_from": "BOI",
+            "fly_to": IATA,
+            "date_from": date,
+            "date_to": date_to,
+            "select_airlines": "NK",
+            "select_airlines_exclude": "True",
+            "adults": 1,
+            "children": 0,
+            "max_stopovers": 2,
+            "stopover_from": "00:00",
+            "stopover_to": "04:00",
+            "curr": "USD",
+
+        }
+        async with aiohttp.ClientSession() as session:
+            response = await session.get(url=f"{flightSearch.url}/v2/search", params=params, headers=self.header, ssl=False)
+            data = await response.json()
+        new_data = await self.analyze_data(data)
+        return new_data
+
+
+    async def expensive_search(self, IATA):
+        date = dt.now().strftime("%d/%m/%Y")
+        six_month = dt.now() + td(days=+ 180)
+        date_to = six_month.strftime("%d/%m/%Y")
+        params = {
+            "fly_from": "BOI",
+            "fly_to": IATA,
+            "date_from": date,
+            "date_to": date_to,
+            "selected_cabins": "C",
+            "mix_with_cabins": "F",
+            "select_airlines": "NK",
+            "select_airlines_exclude": "True",
+            "adults": 1,
+            "children": 0,
+            "max_stopovers": 2,
+            "stopover_from": "00:00",
+            "stopover_to": "04:00",
+            "curr": "USD",
+
+        }
+        async with aiohttp.ClientSession() as session:
+            response = await session.get(url=f"{flightSearch.url}/v2/search", params=params, headers=self.header, ssl=False)
+            data = await response.json()
+        new_data = await self.analyze_data(data)
+        return new_data
+
+    async def analyze_data(self, data):
+        if not data or "data" not in data or not data["data"]:
+            IATA = self.IATA()
+            data = await self.average_search(IATA)
+            if not data or "data" not in data or not data["data"]:
+                IATA = self.IATA()
+                data = await self.cheapest_search(IATA)
+                if not data or "data" not in data or not data["data"]:
+                    # Handle the case where all the search fails
+                    return None, None, None, None, None, None
+
+        cheapest = float('inf')  # Initialize with positive infinity so that any price will be lower
+        cheapest_index = None
+        deep_link = ""
+        total_layover_time = 0
+        total_flight_time = 0
+        for i, flight_data in enumerate(data["data"]):
+            price = flight_data["price"]
+            if price < cheapest:
+                cheapest = price
+                cheapest_index = i
+        stop = data["data"][cheapest_index]["route"]
+        for i in range(len(stop) - 1):  # Stop iterating at the second-to-last segment
+            current_flight_arrival = stop[i]['utc_arrival']
+            next_flight_departure = stop[i + 1]['utc_departure']
+
+            # Convert departure and arrival times to datetime objects for calculation
+            current_flight_arrival_dt = dt.strptime(current_flight_arrival, "%Y-%m-%dT%H:%M:%S.%fZ")
+            next_flight_departure_dt = dt.strptime(next_flight_departure, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+            # Calculate layover time between consecutive flights
+            layover_time = (next_flight_departure_dt - current_flight_arrival_dt).total_seconds()
+
+            # Add layover time to total layover
+            total_layover_time += layover_time
+
+            # Calculate flight time for the current segment
+            flight_time = (next_flight_departure_dt - current_flight_arrival_dt).total_seconds()
+
+            # Add flight time to total flight time
+            total_flight_time += flight_time
+
+            # Convert total layover time to hours and round up
+        total_layover_hours = total_layover_time / 3600
+        total_layover_hours_rounded = round(total_layover_hours)
+
+        # Convert total flight time to hours and round up
+        total_flight_hours = total_flight_time / 3600
+        total_flight_hours_rounded = round(total_flight_hours)
+        total_travel_time = total_flight_hours_rounded + total_layover_hours_rounded
+
+        # Print total layover time after the loop
+        fare = stop[0]["fare_category"]
+        flight_class = ""
+        if fare == "M":
+            flight_class = "Economy"
+        elif fare == "C":
+            flight_class = "Business"
+        elif fare == "F":
+            flight_class = "First Class"
+        stops = len(stop) - 1
+
+        deep_link = data["data"][cheapest_index]["deep_link"]
+
+        return cheapest, flight_class, stops, total_layover_hours_rounded, total_travel_time, deep_link
+
+
+    # async def custom_search(self):
+    #     params = {
+    #         "fly_from": "boise"
+    #     }
+    #     async with aiohttp.ClientSession() as session:
+    #         response = await session.get(url=f"{flightSearch.url}/search", params=params, headers=self.header)
+    #         data = await response.json()
+
+
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 import datetime
-from API import LocationSearch, openAi
+from API import LocationSearch, openAi, flightSearch
 import asyncio
 from aiohttp import ClientSession
 
@@ -51,6 +51,7 @@ async def imgQuery(city):
 async def aIQuery(city):
     gpt = openAi(city)
     country = gpt.aiResponseCountry()
+    airport = gpt.aiResponseAirport()
     dynamic_p1 = gpt.aiResponseP1()
     dynamic_p2 = gpt.aiResponseP2()
     dynamic_p3 = gpt.aiResponseP3()
@@ -58,7 +59,15 @@ async def aIQuery(city):
     dynamic_h2 = gpt.aiResponseH2()
     dynamic_h3 = gpt.aiResponseH3()
 
-    return country, dynamic_p1, dynamic_p2, dynamic_p3, dynamic_h1, dynamic_h2, dynamic_h3
+    return country, airport, dynamic_p1, dynamic_p2, dynamic_p3, dynamic_h1, dynamic_h2, dynamic_h3
+
+
+async def fetch_flight_data(airport, search_func):
+    flightsearch = flightSearch(airport)
+    IATA = flightsearch.IATA()
+    data = await search_func(IATA)
+    return (data[0], data[1], data[2], data[3], data[4], data[5])  # Return flight data as a tuple
+
 
 # Current load time is 20 seconds
 @app.route("/search", methods=["GET", "POST"])
@@ -69,14 +78,34 @@ async def search():
         img_result = await imgQuery(city)
 
         header_img, header_img2, header_img3, base_img, food_img, architecture_img = img_result
-        country, dynamic_p1, dynamic_p2, dynamic_p3, dynamic_h1, dynamic_h2, dynamic_h3 = ai_result
+        country, airport, dynamic_p1, dynamic_p2, dynamic_p3, dynamic_h1, dynamic_h2, dynamic_h3 = ai_result
+
+        flightsearch = flightSearch(airport)
+        c_price, c_fare, c_stops, c_layover, c_time, c_link = await fetch_flight_data(airport,
+                                                                                      flightsearch.cheapest_search)
+        a_price, a_fare, a_stops, a_layover, a_time, a_link = await fetch_flight_data(airport, flightsearch.average_search)
+
+        e_price, e_fare, e_stops, e_layover, e_time, e_link = await fetch_flight_data(airport,flightsearch.expensive_search)
 
     return render_template("search.html", city=city, header_img=header_img, header_img2=header_img2,header_img3=header_img3, base_img=base_img, food_img=food_img,
                                architecture_img=architecture_img, country=country, dynamic_p1=dynamic_p1, dynamic_p2=dynamic_p2, dynamic_p3=dynamic_p3, dynamic_h1=dynamic_h1,
-                                dynamic_h2=dynamic_h2, dynamic_h3=dynamic_h3)
+                                dynamic_h2=dynamic_h2, dynamic_h3=dynamic_h3, c_price=c_price, c_fare=c_fare,c_stops=c_stops,c_layover=c_layover,c_link=c_link, c_time=c_time,
+                           a_price=a_price, a_fare=a_fare, a_stops=a_stops,a_layover=a_layover,a_time=a_time,a_link=a_link, e_price=e_price, e_fare=e_fare, e_stops=e_stops,
+                           e_layover=e_layover,e_time=e_time,e_link=e_link)
+
 
 @app.route("/test", methods=["GET", "POST"])
-def test():
+async def test():
+    flightsearch = flightSearch("Tokyo")
+    IATA = flightsearch.IATA()
+    cheapest_flight_data = await flightsearch.cheapest_search(IATA)
+    price = cheapest_flight_data[0]
+    fare = cheapest_flight_data[1]
+    stops = cheapest_flight_data[2]
+    layover = cheapest_flight_data[3]
+    time = cheapest_flight_data[4]
+    link = cheapest_flight_data[5]
+    print(time)
     return render_template("test.html")
 
 
@@ -85,19 +114,23 @@ def edit_flight():
     return render_template("edit-flight.html")
 
 
-@app.route("/flight", methods=["GET", "POST"])
-async def flight():
-    if request.method == "POST":
-        city = request.form["flying-to"]
-        ai_result = await aIQuery(city)
-        img_result = await imgQuery(city)
-
-        header_img, header_img2, header_img3, base_img, food_img, architecture_img = img_result
-        country, dynamic_p1, dynamic_p2, dynamic_p3, dynamic_h1, dynamic_h2, dynamic_h3 = ai_result
-
-    return render_template("search.html", city=city, header_img=header_img, header_img2=header_img2,header_img3=header_img3, base_img=base_img, food_img=food_img,
-                               architecture_img=architecture_img, country=country, dynamic_p1=dynamic_p1, dynamic_p2=dynamic_p2, dynamic_p3=dynamic_p3, dynamic_h1=dynamic_h1,
-                                dynamic_h2=dynamic_h2, dynamic_h3=dynamic_h3)
+# @app.route("/flight", methods=["GET", "POST"])
+# async def flight():
+#     if request.method == "POST":
+#         city = request.form["flying-to"]
+#         city_from =
+#         ai_result = await aIQuery(city)
+#         img_result = await imgQuery(city)
+#         flightsearch = flightSearch(city)
+#         IATA = flightsearch.IATA()
+#         IATA_TO = flightsearch.IATA()
+#
+#         header_img, header_img2, header_img3, base_img, food_img, architecture_img = img_result
+#         country, dynamic_p1, dynamic_p2, dynamic_p3, dynamic_h1, dynamic_h2, dynamic_h3 = ai_result
+#
+#     return render_template("search.html", city=city, header_img=header_img, header_img2=header_img2,header_img3=header_img3, base_img=base_img, food_img=food_img,
+#                                architecture_img=architecture_img, country=country, dynamic_p1=dynamic_p1, dynamic_p2=dynamic_p2, dynamic_p3=dynamic_p3, dynamic_h1=dynamic_h1,
+#                                 dynamic_h2=dynamic_h2, dynamic_h3=dynamic_h3)
 if __name__ == "__main__":
     app.run(debug=True)
 
